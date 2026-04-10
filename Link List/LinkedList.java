c064bf5bbafcfcd389cc4a8f64338a052ea0f604
Aditya Raj <araj67995@gmail.com>
2026-04-10 23:56:48 +0530

    some operation on linkList

--- Link List/LinkedList.java
+++ Link List/LinkedList.java
@@ -3,18 +3,21 @@ public class LinkedList {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
+    public static int size;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
         // step 1 = create new node
         Node newNode = new Node(data);
-        if(head == null){
+        size++;
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -26,9 +29,10 @@ public class LinkedList {
         head = newNode;
     }
 
-    public void addLast(int data){
+    public void addLast(int data) {
         Node newNode = new Node(data);
-        if(head == null){
+        size++;
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -36,19 +40,78 @@ public class LinkedList {
         tail = newNode;
     }
 
-    public void print(){
-        if(head == null){
+    public void add(int index, int data){
+        if(index == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+         int i = 0;
+        while( i< index-1){
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
             System.out.println("ll is empty");
             return;
         }
         Node temp = head;
-        while(temp != null){
-            System.out.print(temp.data+"->");
+        while (temp != null) {
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("null");
     }
-    public static void main(String args[]){
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size == 1){
+            int val = head.data;
+            size = 0;
+            head = tail = null;
+            return val;
+        }
+
+        int val = head.data;
+        size--;
+        head = head.next;
+        return val;
+    }
+
+    public int removeLast(){
+         if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size == 1){
+            int val = head.data;
+            size = 0;
+            head = tail = null;
+            return val;
+        }
+        // prev: i = size -2
+        Node prev = head;
+        for(int i = 0; i<size-2;i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.print();
         ll.addFirst(2);
@@ -57,9 +120,15 @@ public class LinkedList {
         ll.print();
         ll.addLast(3);
         ll.print();
-        ll.addLast(4);  
-        ll.print();  
-
+        ll.addLast(4);
+        ll.print();
+        ll.add(2, 9);
+        ll.print();
+        System.out.println(ll.size);
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
     }
-    
+
 }
