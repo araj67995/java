ff2f8a15daa4af58cef7f194a97387ac19a05329
Aditya Raj <araj67995@gmail.com>
2026-04-14 23:04:28 +0530

    search in link list 2

--- Link List/LinkedList.java
+++ Link List/LinkedList.java
@@ -128,6 +128,26 @@ public class LinkedList {
         return -1;        
     }
 
+    public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+
+        return idx+1;
+    }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         ll.print();
@@ -148,7 +168,7 @@ public class LinkedList {
         // ll.print();
 
         System.out.println(ll.itrSearch(9));
-        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.recSearch(3));
     }
 
 }
